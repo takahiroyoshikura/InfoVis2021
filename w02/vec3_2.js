23c6da645af41d43111fdd6441045f5612894893
takahiroyoshikura <junchron@icloud.com>
2021-04-20 00:14:23 +0900

    example07

--- w02/vec3_2.js
+++ w02/vec3_2.js
@@ -5,3 +5,16 @@ Vec3 = function( x, y, z )
        this.y = y;
        this.z = z;
 }
+// Add method
+Vec3.prototype.add = function( v )
+{
+	this.x += v.x;
+	this.y += v.y;
+	this.z += v.z;
+ return this;
+}
+// Sum method
+Vec3.prototype.sum = function()
+{
+	return this.x + this.y + this.z;
+}
